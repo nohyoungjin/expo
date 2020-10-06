8f9967ade63e00dd2cbc73b55c0564dda1b1cc56
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-10-06 17:14:15 +0900

    Update 1

---
+++ inc/header.js
@@ -0,0 +1,17 @@
+str = ''
+str += '<header class="header">'
+str += ''
+str += '	<h1 class="logo"><a href="javascript:"><img src="resources/img/common/adminlogo.png" alt=""></a></h1>'
+str += '	<div class="gnb">'
+str += '		<p class="txtGreeting"><strong class="em-weight">expo 관리자</strong>님! 안녕하세요.</p>'
+str += '		<div class="cookie">'
+str += '			<a href="javascript:" class="btn1-xs-slight2">정보수정</a>'
+str += '			<a href="login.html" class="btn1-xs-slight2">로그아웃</a>'
+str += '		</div>'
+str += '	</div>'
+str += ''
+str += '	<hr />'
+str += ''
+str += '</header>'
+
+document.write(str);
